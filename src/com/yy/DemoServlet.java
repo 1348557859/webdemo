4671903dbe82cc9179cbf697d28b33f51ea51c78
pengyy <1348557859@qq.com>
2019-10-04 14:25:41 +0800

    webdemo 01

---
+++ src/com/yy/DemoServlet.java
@@ -0,0 +1,21 @@
+package com.yy;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/demo")
+public class DemoServlet extends HttpServlet {
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        System.out.println("DemoServlet.service");
+        System.out.println("---------------------");
+        req.getRequestDispatcher("/WEB-INF/hello.jsp").forward(req, resp);
+        System.out.println("test for git");
+
+    }
+
+}
---
+++ web/WEB-INF/hello.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: x1c
+  Date: 2019/10/2
+  Time: 19:57
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>hello</title>
+</head>
+<body>
+    hello world !!!
+</body>
+</html>
